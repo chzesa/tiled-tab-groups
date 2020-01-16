fd512ef3f0fee5bc27dda56e215e3899c2573681
chzesa <chzesa+git@gmail.com>
2020-01-16 20:30:49 +0200

    Fix stashed group display toggle button; todo: use css fill for svg icons

--- src/js/view.js
+++ src/js/view.js
@@ -136,18 +136,14 @@ async function initView() {
 		})
 	});
 
-	let displayAllGroupsButton = document.getElementById('visibility-toggle-button');
-	displayAllGroupsButton.addEventListener('click', async () => {
+	document.getElementById('visibility-toggle-button').addEventListener('click', async () => {
 		STATE.drawAllGroups = !STATE.drawAllGroups;
 		await fillGroupNodes();
 		if (STATE.drawAllGroups) {
-			displayAllGroupsButton.style.backgroundImage = `url(icons/eye.svg)`;
 			await TABINTERFACE.forEach(async function (tab) {
 				updateTabNode(tab);
 				if (use_indent) updateIndent(tab.id);
 			}, WINDOW_ID);
-		} else {
-			displayAllGroupsButton.style.backgroundImage = `url(icons/eye-off.svg)`;
 		}
 	});
 
